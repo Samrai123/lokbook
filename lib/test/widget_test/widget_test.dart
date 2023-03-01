85523a7982453e133509b7e5db80081747320223
Samrai123 <raisambhrant71@gmail.com>
2023-03-02 05:12:15 +0545

    widge_tesing_1

---
+++ lib/test/widget_test/widget_test.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:my_second_app/screen/homepage.dart';
+
+void main() {
+  testWidgets('For you page', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: HomeScreen(),
+      ),
+    );
+    Finder title = find.text("For You");
+    expect(title, findsOneWidget);
+  });
+}
