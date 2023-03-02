2569a578cd1b35c20c9a6286db8c8cf0ede51220
Samrai123 <raisambhrant71@gmail.com>
2023-03-02 08:08:44 +0545

    widget_testing2

--- lib/test/widget_test/widget_test.dart
+++ lib/test/widget_test/widget_test.dart
@@ -3,7 +3,10 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:my_second_app/screen/auth1/login.dart';
+import 'package:my_second_app/screen/category/search_page.dart';
 import 'package:my_second_app/screen/homepage.dart';
+import 'package:my_second_app/screen/profile_screen.dart';
+import 'package:my_second_app/screen/visual/post_screen.dart';
 
 void main() {
   testWidgets('For you page', (WidgetTester tester) async {
@@ -13,6 +16,22 @@ void main() {
       ),
     );
     Finder title = find.text("For You");
+    expect(title, findsOneWidget);
+  });
+
+  testWidgets("Search", (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: SearchScreen(),
+    ));
+    Finder title = find.text("Search");
+    expect(title, findsOneWidget);
+  });
+
+  testWidgets("Profiel page", (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: ProfileScreen(),
+    ));
+    Finder title = find.text("Profile Page");
     expect(title, findsOneWidget);
   });
 }
